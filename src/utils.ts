701fa8143662cc8de1ac4aa4438a9e4c3da934bb
Sergio Alfaro <sergioalfaro2807@gmail.com>
2025-11-12 22:55:30 -0600

    Tags page and Search basic structure

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
+
 export function formatDate(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
@@ -6,4 +9,23 @@ export function formatDate(date: Date): string {
   };
 
   return new Date(date).toLocaleDateString(undefined, options);
+}
+
+export function capitalize(str: string): string {
+  if (typeof str !== "string" || str.length === 0) return str;
+
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export async function getAllBlogArticles(): Promise<CollectionEntry<"blog">[]> {
+  return await getCollection("blog");
+}
+
+export async function getAllBlogArticlesSorted(): Promise<
+  CollectionEntry<"blog">[]
+> {
+  return (await getCollection("blog")).sort(
+    (a: CollectionEntry<"blog">, b: CollectionEntry<"blog">) =>
+      b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
+  );
 }
